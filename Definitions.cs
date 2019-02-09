723fbd31b3d8cdd98ecc541747a0131bfe06368f
Tamschi <MailTamschi@googlemail.com>
2019-02-09 11:51:52 +0100

    Initial commit

---
+++ Definitions.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+
+namespace TQ.Common
+{
+    public static class Definitions
+    {
+        //TODO: Should be codepage 1252, but that's not supported by default.
+        public static Encoding Encoding => Encoding.ASCII;
+    }
+}
